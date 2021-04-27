4e98f17415787bf1284559ba8290177d3935c726
Jvidaurri77 <vidaurri77.jv@gmail.com>
2021-04-27 12:17:27 -0500

    Added parsing feature

--- scanner.py
+++ scanner.py
@@ -1,9 +1,26 @@
-## Project 1 - Scanner
-## Jeremy Vidaurri
+## Project 2 - Parsing
+## Jeremy Vidaurri & Ryan Hernandez
 ## CS 3361
 
-import sys
 
+
+import sys
+from os import system,name
+
+
+class parseError(Exception):
+    pass
+
+
+def clear():
+  
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 #Linked List node
 class node:
     #Function to initialize node object
@@ -49,21 +66,29 @@ class linkedList:
         tList = tList + ")"
         print(tList)
             
-class State(linkedList):
+class State(linkedList,parseError):
     #Function to initialize the states. Finals holds all of the accept states. currState describes the state that the DFA is currently at.
-    def __init__(self):
+    def __init__(self,stringArr):
         self.finals = [2,6,7,8,9,10,12,14,15,16,17,18]
         self.currState = 1 #Initialize to 1 as that is where the DFA begins.
+        self.tokenList = linkedList()
+        self.pos = 0
+        self.indentation = 0
+        inputToken = ""
+        self.stringToken = ""
+        self.stringArray = stringArr
+        
+
 
     #Function to check if the token is done
-    def finalState(self,pos,stringArray):
+    def finalState(self,):
         #Check each final state and see if it matches the current state
         for x in self.finals:
             if x == self.currState:
                 #If it is in state 2, it will be a final state if the next character is not / or *
                 if x == 2:
                     try:
-                        if stringArray[pos + 1] == "/" or stringArray[pos + 1] == "*":
+                        if self.stringArray[self.pos + 1] == "/" or self.stringArray[self.pos + 1] == "*":
                             return False
                         else:
                             return True
@@ -75,7 +100,7 @@ class State(linkedList):
                 #If it is in state 14, it will be a final state if the next character is not a digit or a period.
                 elif x == 14:
                     try:
-                        if stringArray[pos + 1].isnumeric() or stringArray[pos + 1] == ".":
+                        if self.stringArray[self.pos + 1].isnumeric() or self.stringArray[self.pos + 1] == ".":
                             return False
                         else:
                             return True
@@ -84,7 +109,7 @@ class State(linkedList):
                 #If it is in state 15, it will be a final state if the next character is not a digit.
                 elif x == 15:
                     try:
-                        if stringArray[pos + 1].isnumeric() :
+                        if self.stringArray[self.pos + 1].isnumeric() :
                             return False
                         else:
                             return True
@@ -93,7 +118,7 @@ class State(linkedList):
                 #If it is in state 16, it will be a final state if the next character is not a letter or digit.
                 elif x == 16:
                     try:
-                        if stringArray[pos + 1].isalnum():
+                        if self.stringArray[self.pos + 1].isalnum():
                             return False
                         else:
                             return True
@@ -103,9 +128,7 @@ class State(linkedList):
         return False
 
     #Function to scan the file
-    def scan(self,stringArray):
-        #Token list to print at the end
-        tokenList = linkedList()
+    def scan(self):
         #Dictionary to differentiate accept states.
         stateDict = {
             2: "div",
@@ -117,12 +140,12 @@ class State(linkedList):
             12: "assign",
             14: "number",
             15: "number",
-            16: "id or keyword",
+            16: "id",
             17: "read",
             18: "write"
         }
+        self.currState = 1
         token = ""
-        pos = 0
         ##Token Flag
         ##0 - No flag
         ##1 - 'read' or 'write' token
@@ -130,146 +153,320 @@ class State(linkedList):
         ##-1 - error
         tokFlag = 0
 
-        while pos < len(stringArray):
+        while self.pos < len(self.stringArray):
             #Comment flag is used to determine if within a comment a newline is met
             commentFlag = 0
             ##STATE 1
             if self.currState == 1:
-                if stringArray[pos] == "/":
+                if self.stringArray[self.pos] == "/":
                     self.currState = 2
-                elif stringArray[pos] == "(":
+                elif self.stringArray[self.pos] == "(":
                     self.currState = 6
-                elif stringArray[pos] == ")":
+                elif self.stringArray[self.pos] == ")":
                     self.currState = 7
-                elif stringArray[pos] == "+":
+                elif self.stringArray[self.pos] == "+":
                     self.currState = 8
-                elif stringArray[pos] == "-":
+                elif self.stringArray[self.pos] == "-":
                     self.currState = 9
-                elif stringArray[pos] == "*":
+                elif self.stringArray[self.pos] == "*":
                     self.currState = 10
-                elif stringArray[pos] == ":":
+                elif self.stringArray[self.pos] == ":":
                     self.currState = 11
-                elif stringArray[pos] == ".":
+                elif self.stringArray[self.pos] == ".":
                     self.currState = 13
-                elif stringArray[pos].isnumeric():
+                elif self.stringArray[self.pos].isnumeric():
                     self.currState = 14
-                elif stringArray[pos] == "r" and pos + 3 < len(stringArray) and stringArray[pos + 1] == "e" and stringArray[pos + 2] == "a" and stringArray[pos + 3] == "d":
+                elif self.stringArray[self.pos] == "r" and self.pos + 3 < len(self.stringArray) and self.stringArray[self.pos + 1] == "e" and self.stringArray[self.pos + 2] == "a" and self.stringArray[self.pos + 3] == "d":
                     self.currState = 17
-                    pos += 4
+                    self.pos += 4
                     token = "read"
                     tokFlag = 1
-                elif stringArray[pos] == "w" and pos + 4 < len(stringArray) and stringArray[pos + 1] == "r" and stringArray[pos + 2] == "i" and stringArray[pos + 3] == "t" and stringArray[pos + 4] == "e":
+                elif self.stringArray[self.pos] == "w" and self.pos + 4 < len(self.stringArray) and self.stringArray[self.pos + 1] == "r" and self.stringArray[self.pos + 2] == "i" and self.stringArray[self.pos + 3] == "t" and self.stringArray[self.pos + 4] == "e":
                     self.currState = 18
-                    pos += 5
+                    self.pos += 5
                     token = "write"
                     tokFlag = 1
-                elif stringArray[pos].isalpha():
+                elif self.stringArray[self.pos].isalpha():
                     self.currState = 16
-                elif stringArray[pos] == ' ' or stringArray[pos] == "\n":
+                elif self.stringArray[self.pos] == ' ' or self.stringArray[self.pos] == "\n":
                     self.currState = 1
                     tokFlag = 2
                 else:
                     tokFlag = -1
-                    break
+                    
                     
             ##STATE 2
             elif self.currState == 2:
                 token = ""
-                if stringArray[pos] == "/":
+                if self.stringArray[self.pos] == "/":
                     self.currState = 3
                     tokFlag = 2
-                    while(pos+1 <= len(stringArray)-1 or commentFlag != 1):
-                        pos +=1
-                        if stringArray[pos] == "\n":
+                    while(self.pos+1 <= len(self.stringArray)-1 or commentFlag != 1):
+                        self.pos +=1
+                        if self.stringArray[self.pos] == "\n":
                             self.currState = 1
                             commentFlag = 1
-                elif stringArray[pos] == "*":
+                elif self.stringArray[self.pos] == "*":
 
                     self.currState = 4
                     tokFlag = 2
-                    while(pos+2 <= len(stringArray)-1 and commentFlag != 1):
-                        pos +=1
-                        if self.currState == 4 and stringArray[pos] == "*":
+                    while(self.pos+2 <= len(self.stringArray)-1 and commentFlag != 1):
+                        self.pos +=1
+                        if self.currState == 4 and self.stringArray[self.pos] == "*":
                             self.currState = 5
-                        elif self.currState == 5 and stringArray[pos] == "/":
+                        elif self.currState == 5 and self.stringArray[self.pos] == "/":
                             self.currState = 1
                             commentFlag = 1
                         elif self.currState == 5:
                             self.currState == 4
-                    pos +=1
+                    self.pos +=1
                 else:
                     tokFlag = -1
-                    break
+                    
                 
             ##STATE 11
             elif self.currState == 11:
-                if stringArray[pos] == "=":
+                if self.stringArray[self.pos] == "=":
                     self.currState = 12
                 else:
                     tokFlag = -1
-                    break
+                    
 
             ##STATE 13
             elif self.currState == 13:
-                if stringArray[pos].isnumeric():
+                if self.stringArray[self.pos].isnumeric():
                     self.currState = 15
                 else:
                     tokFlag = -1
-                    break
+                    raise parseError
 
             ##STATE 14
             elif self.currState == 14:
-                if stringArray[pos].isnumeric():
+                if self.stringArray[self.pos].isnumeric():
                     self.currState = 14
-                elif stringArray[pos] == ".":
+                elif self.stringArray[self.pos] == ".":
                     self.currState = 15
                 else:
                     tokFlag = -1
-                    break
+                    raise parseError
 
             ##STATE 15
             elif self.currState == 15:
-                if stringArray[pos].isnumeric():
+                if self.stringArray[self.pos].isnumeric():
                     self.currState = 15
                 else:
                     tokFlag = -1
-                    break
+                    raise parseError
 
             ##STATE 16
             else:
-                if stringArray[pos].isalnum():
+                if self.stringArray[self.pos].isalnum():
                     self.currState = 16
                 else:
                     tokFlag = -1
-                    break
+                    raise parseError
 
             ##Check to see if it is in a final state. If so, check the tokFlag to see if anything needs to be added to the token. Reset the token, flag, and currState.
-            if self.finalState(pos,stringArray):
+            if self.finalState():
 
                 if tokFlag != 1:
-                    token = token + stringArray[pos]
-                tokenList.addNode(token,stateDict[self.currState])
-                token = ""
-                tokFlag = 2
-                self.currState = 1
+                    token = token + self.stringArray[self.pos]
+
+                self.pos += 1
+                #print("\t\t" + token + ": " + stateDict[self.currState])
+                return token,stateDict[self.currState]
+                
             
             #Flag 0, normal character
             if tokFlag == 0:
-                token = token + stringArray[pos]
-                pos += 1
+                token = token + self.stringArray[self.pos]
+                self.pos += 1
             #Flag 1, read or write token
             elif tokFlag == 1:
                 tokFlag = 0
             #Flag 2, comment or final state
             elif tokFlag == 2:
                 tokFlag = 0
-                pos += 1
+                self.pos += 1
+        else:
+            return ("$$","$$")
                 
         #If tokFlag is -1, there was an error with the tokens. Print only an error. Otherwise, print the list of tokens.
         if tokFlag == -1:
                 print("Error.")
-        else:        
-            tokenList.printList()
+
+    def match(self,token,inputToken):
+        
+        if inputToken == token and not inputToken =="$$":
+            self.indentation+=1
+            print("\t"*self.indentation+"<"+inputToken+">")
+            self.indentation+=1
+            print("\t"*self.indentation + self.stringToken) ##FIX STRING TOKEN
+            self.indentation-=1
+            print("\t"*self.indentation+"</"+inputToken+">")
+            self.indentation-=1
+        elif inputToken =="$$":
+            return
+        else: 
+            raise parseError
+
+
+    def program(self):
+        self.stringToken, inputToken = self.scan() 
+        if inputToken == "id" or inputToken == "read" or inputToken == "write":
+            print("<Program>")
+            self.stmt_list(inputToken)
+            self.stringToken, inputToken = self.scan() 
+            self.match("$$",inputToken)
+            print("</Program>")
+        else:
+            raise parseError()
+        
+    def stmt_list(self,inputToken):
+        if inputToken == "id" or inputToken == "read" or inputToken == "write":
+            self.indentation += 1
+            print("\t"*self.indentation + "<stmt_list>")
+            self.stmt(inputToken)
+            self.stringToken, inputToken = self.scan() 
+            self.stmt_list(inputToken)
+            print("\t"*self.indentation + "</stmt_list>")
+            self.indentation -= 1
+
+        elif inputToken == "$$":
+            self.indentation += 1
+            print("\t"*self.indentation + "<stmt_list>")
+            print("\t"*self.indentation + "</stmt_list>")
+            self.indentation -= 1
+            return
+        else:
+            print("stmt_list error " + inputToken)
+            raise parseError()
+    
+
+    def stmt(self, inputToken):
+        if inputToken == "id":
+            self.indentation += 1
+            print("\t"*self.indentation + "<stmt>")
+            self.match("id",inputToken)
+            self.stringToken, inputToken = self.scan() 
+            self.match("assign",inputToken)
+            self.expr(inputToken)
+            print("\t"*self.indentation + "</stmt>")
+            self.indentation -= 1
+        elif inputToken =="read":
+            self.indentation += 1
+            print("\t"*self.indentation + "<stmt>")
+            self.match("read",inputToken)
+            self.stringToken, inputToken = self.scan() 
+            self.match("id",inputToken)
+            print("\t"*self.indentation + "</stmt>")
+            self.indentation -= 1
+        elif inputToken == "write":
+            self.indentation += 1
+            print("\t"*self.indentation + "<stmt>")
+            self.match("write",inputToken)
+            self.expr(inputToken)
+            print("\t"*self.indentation + "</stmt>")
+            self.indentation -= 1
+        else:
+            raise parseError()
+
+    def expr(self,inputToken):
+        self.stringToken, inputToken = self.scan() 
+        if inputToken == "id" or inputToken == "number" or inputToken=="lparen":
+            self.indentation +=1
+            print("\t"*self.indentation +"<expr>")
+            self.term(inputToken)
+            self.term_tail(inputToken)
+            print("\t"*self.indentation +"</expr>")
+            self.indentation -=1
+        else:
+            raise parseError()
+
+    def term_tail(self,inputToken):
+        self.stringToken, inputToken = self.scan()
+        self.indentation+=1
+        print("\t"*self.indentation +"<term_tail>")
+        if inputToken =="plus" or inputToken =="minus":
+            
+            self.add_op(inputToken)
+            self.term(inputToken)
+            self.term_tail(inputToken)
+            print("\t"*self.indentation +"</term_tail>")
+            self.indentation-=1
+        elif inputToken =="rparen" or inputToken =="id" or inputToken =="read" or inputToken =="write" or inputToken=="$$":
+            print("\t"*self.indentation +"</term_tail>")
+            self.indentation-=1    
+            return
+        else:
+            raise parseError()
+
+    def term(self,inputToken):        
+        if inputToken == "id" or inputToken == "number" or inputToken == "lparen":
+            self.indentation+=1
+            print("\t"*self.indentation +"<term>")
+            self.factor(inputToken)
+            self.factor_tail(inputToken)
+            print("\t"*self.indentation +"</term>")
+            self.indentation-=1
+
+        else:
+            raise parseError(inputToken)
+
+    def factor_tail(self,inputToken):
+        self.stringToken, inputToken = self.scan()
+        self.indentation +=1
+        print("\t"*self.indentation +"<factor_tail>")
+        if inputToken == "multiply" or inputToken == "div":
+            self.mult_op(inputToken)
+            self.factor(inputToken)
+            self.factor_tail(inputToken)
+            print("\t"*self.indentation +"</factor_tail>")
+            self.indentation-=1
+        elif inputToken == "plus" or inputToken == "minus" or inputToken == "rparen" or inputToken == "id" or inputToken == "read" or inputToken == "write" or inputToken == "$$":
+            print("\t"*self.indentation +"</factor_tail>")
+            self.indentation-=1
+            return
+        else:
+            raise parseError(inputToken)
+
+    def factor(self,inputToken):
+        self.indentation+=1
+        print("\t"*self.indentation +"<factor>")
+        if inputToken == "id":
+            self.match("id",inputToken)
+        elif inputToken == "number":
+            self.match("number",inputToken)
+        elif inputToken == "lparen":
+            self.match("lparen",inputToken)
+            self.expr(inputToken)
+            self.match("rparen",inputToken)
+        else:
+            raise parseError()
+        print("\t"*self.indentation +"</factor>")
+        self.indentation-=1
+
+    def add_op(self,inputToken):
+        print("add_op")
+        if inputToken == "plus":
+            self.match("plus",inputToken)
+        elif inputToken == "minus":
+            self.match("minus",inputToken)
+        else:
+            raise parseError()
+            
+    def mult_op(self,inputToken):
+        print("mult_op")
+        if inputToken == "multiply":
+            self.match("multiply",inputToken)
+        elif inputToken == "div":
+            self.match("div",inputToken)
+        else:
+            raise parseError()
+
+    def parse(self,):  
+        while (self.pos < len(self.stringArray) ):
+            self.program()
 
 class File():
 
@@ -289,11 +486,15 @@ class File():
 def main():
     userfile = File() #Grab file from user
     stringArr = userfile.filetoArray() #Store each character in a single array
-    token = State() #State used to begin the scan process
-    token.scan(stringArr) #Scan the file using the array from the file.
+    token = State(stringArr) #State used to begin the scan process
+    token.parse() #Scan the file using the array from the file.
+
 
 #If the provided file does not exist, error out.
 try:
     main()
 except FileNotFoundError:
     print("The specified file does not exist!")
+except parseError:
+    clear()
+    print("Error")
